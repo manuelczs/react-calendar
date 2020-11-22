22bb69f6886f07936f98531462093159c0ba928b
manuel <manuelczs@outlook.com>
2020-11-22 19:24:05 -0300

    Updates

---
+++ precache-manifest.2a25dd28335a7791ea564f0dc96e0f53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f64a4bacf43a311c9c99ea6470ae8ca",
+    "url": "/react-calendar/index.html"
+  },
+  {
+    "revision": "f8b0861a4adbee57634d",
+    "url": "/react-calendar/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "1c7abfb6ea8b48de943d",
+    "url": "/react-calendar/static/css/main.88748a23.chunk.css"
+  },
+  {
+    "revision": "f8b0861a4adbee57634d",
+    "url": "/react-calendar/static/js/2.5c6ce914.chunk.js"
+  },
+  {
+    "revision": "957d8cebf3c578fe6d20836612718610",
+    "url": "/react-calendar/static/js/2.5c6ce914.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c7abfb6ea8b48de943d",
+    "url": "/react-calendar/static/js/main.5e0dfb0c.chunk.js"
+  },
+  {
+    "revision": "cf66104f7026d10895ed",
+    "url": "/react-calendar/static/js/runtime-main.73ae58af.js"
+  }
+]);
