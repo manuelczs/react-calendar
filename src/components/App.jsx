1144081b886676d9d2d98344457a46251d8d1c3f
manuel <manuelczs@outlook.com>
2021-01-31 19:14:47 -0300

    data.json moved to public folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,7 +24,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('./data.json')
+    fetch('https://manux.ar/data.json')
       .then((response) => response.json())
       .then((result) => {
         const apts = result.map((item) => {
